56f62c3fe723dc94de65a3a7d1a5064daea895ac
Saeeda <saeedabatool39@gmail.com>
2024-09-16 10:56:57 -0400

    fixed code so that both the home screen and guest screen displayed their own individual scores and synced scores. Also added in a reset button which will turn the scores back to 0

--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,31 @@
-let count = 0
+let homeScore = 0
+let guestScore = 0 
 
 let homeEl = document.getElementById("home-el")
-
-homeEl.textContent = count
-
-function onePoint() {
-  count += 1
-  homeEl.textContent = count 
-}
-
-function twoPoint() {
-  count += 2
-  homeEl.textContent = count 
-}
-
-function threePoint() {
-  count += 3
-  homeEl.textContent = count 
-}
-
 let guestEl = document.getElementById("guest-el")
+let resetBtn = document.getElementById("reset-btn")
+
+
+homeEl.textContent = homeScore
+guestEl.textContent = guestScore 
+
+// function that takes in points as parameter. When the onlick is invoked, it will see which button was clicked and will add the corresponding points. 
+function homePoint(points) {
+  homeScore += points 
+  homeEl.textContent = homeScore
+}
+
+
+function guestPoint(points) {
+  guestScore += points 
+  guestEl.textContent = guestScore 
+}
+
+
+// reset button which when clicked will turn the home and guest screen back to 0 and will also change the current score of home and guest team back to 0. 
+resetBtn.addEventListener("click", function () {
+  homeEl.textContent = 0 
+  guestEl.textContent = 0 
+  homeScore = 0
+  guestScore = 0
+})
